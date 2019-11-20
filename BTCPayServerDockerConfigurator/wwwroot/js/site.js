502ea50489e74f874c38468527d6c73d9cd22ab5
Kukks <evilkukka@gmail.com>
2019-11-20 16:17:21 +0100

    disable btn when form is submitting

--- BTCPayServerDockerConfigurator/wwwroot/js/site.js
+++ BTCPayServerDockerConfigurator/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+
+    $("form").on("submit", function () {
+        $(this).find("button[type=submit]").attr("disabled", "disabled")
+    });
+});
