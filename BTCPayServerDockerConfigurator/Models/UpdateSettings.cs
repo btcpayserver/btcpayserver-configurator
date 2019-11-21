efdb1ac43a1c488358e2f171eab5b428186d0923
Kukks <evilkukka@gmail.com>
2019-11-21 16:02:29 +0100

    prevent possible null exceptions

--- BTCPayServerDockerConfigurator/Models/UpdateSettings.cs
+++ BTCPayServerDockerConfigurator/Models/UpdateSettings.cs
@@ -1,9 +1,9 @@
 namespace BTCPayServerDockerConfigurator.Models
 {
-    public class UpdateSettings<T,TAdditionalData>
+    public class UpdateSettings<T,TAdditionalData> where TAdditionalData : new()
     {
         public T Settings { get; set; }
-        public TAdditionalData Additional { get; set; }
+        public TAdditionalData Additional { get; set; } = new TAdditionalData();
         public string Json { get; set; }
     }
 }
