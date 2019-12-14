16d335698a13404075f297dc1236a0536b74b574
Kukks <evilkukka@gmail.com>
2019-12-14 15:52:47 +0100

    fix minor things

--- BTCPayServerDockerConfigurator/Validation/RequiredIfAttribute.cs
+++ BTCPayServerDockerConfigurator/Validation/RequiredIfAttribute.cs
@@ -6,11 +6,14 @@ namespace BTCPayServerDockerConfigurator.Validation
     //from https://stackoverflow.com/questions/52321148/conditional-validation-in-mvc-net-core-requiredif
     public class RequiredIfAttribute : ValidationAttribute
     {
+        private readonly bool _notDesiredValue;
         private String PropertyName { get; set; }
         private Object DesiredValue { get; set; }
+        
 
-        public RequiredIfAttribute(String propertyName, Object desiredvalue, String errormessage)
+        public RequiredIfAttribute(String propertyName, Object desiredvalue, String errormessage, bool notDesiredValue = false)
         {
+            _notDesiredValue = notDesiredValue;
             this.PropertyName = propertyName;
             this.DesiredValue = desiredvalue;
             this.ErrorMessage = errormessage;
@@ -21,11 +24,19 @@ namespace BTCPayServerDockerConfigurator.Validation
             Object instance = context.ObjectInstance;
             Type type = instance.GetType();
             Object proprtyvalue = type.GetProperty(PropertyName).GetValue(instance, null);
-            if (proprtyvalue.ToString() == DesiredValue.ToString() && value == null)
+
+            if (value != null)
+            {
+                return ValidationResult.Success;
+            }else if (_notDesiredValue && proprtyvalue.ToString() != DesiredValue.ToString())
+            {
+                return new ValidationResult(ErrorMessage);
+            }else if (!_notDesiredValue && proprtyvalue.ToString() == DesiredValue.ToString())
             {
                 return new ValidationResult(ErrorMessage);
             }
             return ValidationResult.Success;
+           
         }
     }
 }
