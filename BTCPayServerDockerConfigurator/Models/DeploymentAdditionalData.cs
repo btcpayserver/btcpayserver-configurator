428a110ea350d20a9f23c23b94d966be9d7edc06
Kukks <evilkukka@gmail.com>
2020-06-04 09:06:31 +0200

    fix text

--- BTCPayServerDockerConfigurator/Models/DeploymentAdditionalData.cs
+++ BTCPayServerDockerConfigurator/Models/DeploymentAdditionalData.cs
@@ -6,7 +6,7 @@ namespace BTCPayServerDockerConfigurator.Models
     public class DeploymentAdditionalData
     {
         public List<DeploymentType> AvailableDeploymentTypes { get; set; } = new List<DeploymentType>();
-        [Display(Name = "Load existing settings(if possible)")]
+        [Display(Name = "Load existing settings (if possible)")]
         public bool LoadFromServer { get; set; } = true;
     }
 }
